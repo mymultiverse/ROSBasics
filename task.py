e5172348bbb94ee86a9bc9d04d6f485852731991
Nebula <yrajsm@gmail.com>
2018-04-20 18:32:45 +0900

    Update task.py

--- task.py
+++ task.py
@@ -1,4 +1,9 @@
 # python version 
 import rospy
-from geometry_msgs import PoseStamped
-from mavros_msgs import State
+from geometry_msgs.msg import PoseStamped
+from mavros_msgs.msg import State
+from mavros_msgs.srv import SetMode, SetModeRequest, SetModeResponse, CommandBool, CommandBoolRequest, CommandBoolResponse, CommandTOL, CommandTOLRequest
+import time
+from tf.transformations import *
+import numpy as np
+
